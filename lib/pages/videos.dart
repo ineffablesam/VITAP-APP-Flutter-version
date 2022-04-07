90a43197099662353b78fe43df83d2a264ad9879
Sarath191181208 <74459981+Sarath191181208@users.noreply.github.com>
2022-04-07 19:05:26 +0530

    added the core caluculator

--- lib/pages/videos.dart
+++ lib/pages/videos.dart
@@ -54,65 +54,251 @@ class _VideoArticlesState extends State<VideoArticles>
     final vb = context.watch<VideosBloc>();
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        title: Text('video articles').tr(),
-        elevation: 5,
-      ),
-      body: RefreshIndicator(
-        child: vb.hasData == false
-            ? ListView(
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.35,
-                  ),
-                  EmptyPage(
-                      icon: Feather.clipboard,
-                      message: 'no articles found'.tr(),
-                      message1: ''),
-                ],
-              )
-            : ListView.separated(
-                padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
-                controller: controller,
-                physics: AlwaysScrollableScrollPhysics(),
-                itemCount: vb.data.length != 0 ? vb.data.length + 1 : 5,
-                separatorBuilder: (BuildContext context, int index) => SizedBox(
-                  height: 15,
-                ),
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          title: Text('GPA Caluculator').tr(),
+          elevation: 5,
+        ),
+        body: Center(child: GPACaluculator(5)));
+  }
 
-                //shrinkWrap: true,
-                itemBuilder: (_, int index) {
-                  if (index < vb.data.length) {
-                    if (index.isOdd)
-                      return Card4(d: vb.data[index], heroTag: 'video$index');
-                    else
-                      return Card5(
-                        d: vb.data[index],
-                        heroTag: 'video$index',
-                      );
-                  }
-                  return Opacity(
-                    opacity: vb.isLoading ? 1.0 : 0.0,
-                    child: vb.lastVisible == null
-                        ? LoadingCard(height: 250)
-                        : Center(
-                            child: SizedBox(
-                                width: 32.0,
-                                height: 32.0,
-                                child: new CupertinoActivityIndicator()),
+  @override
+  bool get wantKeepAlive => true;
+}
+
+class GPACaluculator extends StatefulWidget {
+  final int n;
+  GPACaluculator(this.n);
+
+  @override
+  State<GPACaluculator> createState() => _GPACaluculatorState();
+}
+
+class _GPACaluculatorState extends State<GPACaluculator> {
+  List<String> _items =
+      ['S', 'A+', 'A', 'B+', 'B', 'C', 'D', 'F/Ab/I'].toList();
+  List<String> _itemsCp = ['1', '2', '3', '4', '5'].toList();
+  var _selection;
+  var _selectionCp;
+  var list;
+
+  @override
+  void initState() {
+    super.initState();
+    _selection = List<String>.filled(widget.n, "0");
+    _selectionCp = List<String>.filled(widget.n, "0");
+    list = new List<int>.generate(widget.n, (i) => i);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int sogxc = 0, soc = 0;
+    var textFields = <Widget>[];
+    bool safeToNavigate = true;
+    textFields.add(
+      new Row(children: [
+        new Padding(
+          padding: new EdgeInsets.only(left: 96.0),
+        ),
+        new Column(children: [
+          new Text(
+            "Grade",
+            overflow: TextOverflow.ellipsis,
+            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+          ),
+        ]),
+        new Padding(
+          padding: new EdgeInsets.only(left: 62.0),
+        ),
+        new Column(
+          children: [
+            new Text(
+              "Credits",
+              overflow: TextOverflow.ellipsis,
+              style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+            ),
+          ],
+        ),
+        new Padding(
+          padding: new EdgeInsets.only(bottom: 25.0),
+        ),
+      ]),
+    );
+    list.forEach((i) {
+      textFields.add(
+        new Column(
+          children: [
+            new Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  new Text(
+                    "Subject ${i + 1}:",
+                    style: new TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.all(10.0),
+                  ),
+                  new DropdownButton<String>(
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    hint: new Text(
+                      "grade ${i + 1}",
+                    ),
+                    value: _selection[i] == "0" ? null : _selection[i],
+                    items: _items.map((String item) {
+                      return new DropdownMenuItem<String>(
+                        value: item,
+                        child: new Text(
+                          item,
+                          textAlign: TextAlign.center,
+                          style: new TextStyle(
+                            color: Colors.purple[400],
                           ),
-                  );
-                },
-              ),
-        onRefresh: () async {
-          context.read<VideosBloc>().onRefresh(mounted, _orderBy);
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (s) {
+                      setState(() {
+                        _selection[i] = s;
+                      });
+                    },
+                  ),
+                  new Padding(
+                    padding: new EdgeInsets.all(25.0),
+                  ),
+                  new DropdownButton<String>(
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    hint: new Text(
+                      "credit ${i + 1}",
+                      style: TextStyle(fontWeight: FontWeight.normal),
+                    ),
+                    value: _selectionCp[i] == "0" ? null : _selectionCp[i],
+                    items: _itemsCp.map((String items) {
+                      return new DropdownMenuItem<String>(
+                        value: items,
+                        child: new Text(
+                          items,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (s) {
+                      setState(() {
+                        _selectionCp[i] = s;
+                      });
+                    },
+                  ),
+                ]),
+          ],
+        ),
+      );
+    });
+
+    double res = 0.0;
+
+    return new Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: new Container(
+        decoration: new BoxDecoration(
+            border: new Border.all(color: Colors.transparent, width: 30.0)),
+        child: new ListView(
+          children: textFields,
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Calculate',
+        backgroundColor: Colors.deepOrangeAccent,
+        child: new Icon(Icons.gavel),
+        onPressed: () {
+          for (int i = 0; i < widget.n; i++) {
+            if (_selectionCp[i] == null) {
+              safeToNavigate = false;
+              continue;
+            }
+            if (_selection[i] == null) {
+              safeToNavigate = false;
+              continue;
+            }
+            int r = int.parse(_selectionCp[i]);
+            int gp = calculate(_selection[i]);
+            int cp = r;
+            int gxc = gp * cp;
+            sogxc += gxc;
+            soc += cp;
+          }
+          res = sogxc / soc;
+          if (safeToNavigate)
+            showRes(res);
+          else {
+            alert();
+          }
         },
       ),
     );
   }
 
-  @override
-  bool get wantKeepAlive => true;
+  int calculate(var a) {
+    if (a == "O") return 10;
+    if (a == "A+") return 9;
+    if (a == "A") return 8;
+    if (a == "B+") return 7;
+    if (a == "B") return 6;
+    if (a == "C") return 5;
+    if (a == "P") return 4;
+    if (a == "F/Ab/I") return 0;
+    return 0;
+  }
+
+  Future<Null> showRes(double num) async {
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('GPA'),
+          content: new SingleChildScrollView(
+            child: Text("$num"),
+          ),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text('retry'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<Null> alert() async {
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('Rewind and remember'),
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                new Text('You have done something terrible.'),
+                new Text('Go back and reflect on your mistakes.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text('retry'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
